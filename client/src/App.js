b3ed900ce9c36645897c448c3f4de103b7ac0b1d
Johnny <eugeni777@yandex.ru>
2024-03-06 21:07:03 +0100

    Testing the deployment to AWS

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
+          Now as a multi-container Docker application in AWS!!!
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
